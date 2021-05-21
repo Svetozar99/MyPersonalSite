6548f63579c700e8cd8511e9b7e73489a46e49f8
Svetozar99 <svetozarbrboric@gmail.com>
2021-05-21 18:48:08 +0200

    commit

---
+++ app.js
@@ -0,0 +1,17 @@
+function clickOnAboutMe(){
+    $("#home").hide();
+    $("#skills").hide();
+    $("#aboutme").show();
+}
+
+function clickOnHome(){
+    $("#aboutme").hide();
+    $("#skills").hide();
+    $("#home").show();
+}
+
+function clickOnSkills(){
+    $("#aboutme").hide();
+    $("#home").hide();
+    $("#skills").show();
+}
